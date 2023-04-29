4144afa791aa265b10837d9da69f9022b720f4d8
KortexDev <cacortesl01@gmail.com>
2023-04-29 16:47:07 -0500

    se agregan estilos al componente buscador

--- src/components/Searcher.tsx
+++ src/components/Searcher.tsx
@@ -4,12 +4,21 @@ import SearchIcon from '@mui/icons-material/Search';
 import React from 'react';
 
 function Searcher():JSX.Element{
-
+    
     return (
-        <Stack direction="row">
-            <SearchIcon/>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-            <IconButton></IconButton>
+        <Stack direction="row" alignItems="center" justifyContent="center" sx={{
+            marginTop: "24px",
+        }}>
+            <TextField id="outlined-basic" label="Github User" variant="outlined" InputProps={{
+                endAdornment: (
+                  <IconButton size="small">
+                    <SearchIcon/>
+                  </IconButton>
+                )
+              }} placeholder='Search user' size='small' color='primary' sx={{
+                borderRadius: "8px",
+                color: "white",
+              }}/>
         </Stack>
     )
 }
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -2,20 +2,33 @@ import { Searcher } from "@/components/Searcher";
 import { Container } from "@mui/material";
 import React from "react";
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#e3f2fd',
+    },
+  },
+});
 
 
 function App(): JSX.Element{
   
     return (  
-      
+      <ThemeProvider theme={theme}>
         <Container sx={{
             width: "90%",
             height: "80vh",
+            borderRadius:"8px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
             background: "#1c1e22",
         }} >
             <Searcher/>
         </Container>
+      </ThemeProvider>
            
     )
 }
